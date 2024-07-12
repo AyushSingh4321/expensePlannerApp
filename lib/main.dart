02bfb67fd5490fa7a9bb3fd12f45caa80ef8af3a
AyushSingh4321 <ayushksb11@gmail.com>
2024-07-12 16:05:56 +0530

    changedAppIcon

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(secondary: Colors.amberAccent),
       ),
-      home:SplashScreen(),
+      home: SplashScreen(),
       routes: {
         MyHomePage.routeName:(ctx) => MyHomePage(),
       },
